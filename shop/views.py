b4232ea270a7038940d9caabf0d27da4ffe6798a
botvkk <vivekt077@gmail.com>
2020-01-07 01:42:52 +0530

    minor changes

--- shop/views.py
+++ shop/views.py
@@ -3,7 +3,7 @@ from .models import Product, Contact, Orders, OrderUpdate
 from math import ceil
 import json
 from django.views.decorators.csrf import csrf_exempt
-from PayTm import Checksum
+
 # Create your views here.
 from django.http import HttpResponse
 MERCHANT_KEY = 'Your-Merchant-Key-Here'
@@ -145,4 +145,4 @@ def handlerequest(request):
             print('order successful')
         else:
             print('order was not successful because' + response_dict['RESPMSG'])
-    return render(request, 'shop/paymentstatus.html', {'response': response_dict})
+    return render(request, 'shop/.html', {'response': response_dict})
